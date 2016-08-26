582b6f613ae9b853e234f4cdecd50484583e2d53
QuDump <xiaoqidongsc@qq.com>
2016-08-26 17:41:09 +0800

    add rxbus

---
+++ app/src/main/java/com/qudump/jiangedan/event/NumberEvent.java
@@ -0,0 +1,16 @@
+package com.qudump.jiangedan.event;
+
+/**
+ * Created by dell on 2016/8/26.
+ */
+public class NumberEvent {
+    private int num;
+
+    public NumberEvent(int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
+}
---
+++ app/src/main/java/com/qudump/jiangedan/ui/RxBusTestActivity.java
@@ -0,0 +1,19 @@
+package com.qudump.jiangedan.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.qudump.jiangedan.R;
+
+/**
+ * Created by dell on 2016/8/26.
+ */
+public class RxBusTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_bus_test);
+    }
+}
